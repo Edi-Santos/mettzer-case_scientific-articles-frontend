e19717dabe79a9d3e5a78be193f3e5fc081e1b39
Ed Santos <edsantos2822@gmail.com>
2022-09-09 00:46:37 -0300

    test:(home.cy.js): implementando teste 1 - testa se há um <header />

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,9 +1,14 @@
 /// <reference types="cypress" />
 
 const URL_HOME = 'http://localhost:3000/home';
+const HEADER = 'header';
 
 describe('Testando Tela Home', () => {
   before(() => {
     cy.visit(URL_HOME);
+  });
+
+  it('1 - testa se há um <header />', () => {
+    cy.get(HEADER).should('exist');
   });
 });
